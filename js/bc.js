b839f201b2e788e9454ed84a702957994803af88
timaobimundial <carlos.gomes@navbrasil.gov.br>
2026-05-01 02:48:17 +0000

    Add files via upload

--- js/bc.js
+++ js/bc.js
@@ -29,7 +29,7 @@ async function buscarAeronavesProximas() {
     const sburLongitude = sbur[0];
     const sburLatitude = sbur[1];
     const raioNM = 70; // Raio de busca em NM
-    const apiUrl = `https://api.adsb.one/v2/point/${sburLatitude}/${sburLongitude}/${raioNM}`;
+    const apiUrl = `https://bc.carlos-gomes-299.workers.dev/?lat=${sburLatitude}&lon=${sburLongitude}&raio=${raioNM}`;
 
     // Exibe o GIF de carregamento
     imagemCarregamento.style.display = 'block';
