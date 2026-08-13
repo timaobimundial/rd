5f305a39fb4569c80063760f4b312772b5aa3d0d
timaobimundial <carlos.gomes@navbrasil.gov.br>
2026-08-13 03:19:37 +0000

    Update airplanes.js

--- api/airplanes.js
+++ api/airplanes.js
@@ -1,7 +1,11 @@
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     try {
-        const response = await fetch('https://api.airplanes.live/v2/point/-19.794722/-47.958611/70');
+        const response = await fetch('https://api.airplanes.live/v2/point/-19.794722/-47.958611/70', {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+            }
+        });
         const data = await response.json();
         res.status(200).json(data);
     } catch (error) {
