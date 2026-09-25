e5f833f460fc29ab2e4cca61a44624879ebbe369
timaobimundial <carlos.gomes@navbrasil.gov.br>
2026-09-25 08:09:33 +0000

    Update bc.js

--- js/bc.js
+++ js/bc.js
@@ -466,7 +466,7 @@ async function buscarAeronavesProximas() {
 
             const nivelDeVooAbaixoDe195 =
                 aircraft.flightLevel != null &&
-                aircraft.flightLevel <= 195;
+                aircraft.flightLevel <= 469;
 
             if (aircraft.dentroPoligono && nivelDeVooAbaixoDe195) {
                 identifierCell.classList.add('dentro-poligono-e-abaixo-f195');
